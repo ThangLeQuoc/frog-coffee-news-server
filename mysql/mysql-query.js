d3635453c22a3e6b27934eb2bf3b6339c6b320e8
Thang Le <silverhair.guy@gmail.com>
2016-11-16 14:56:02 +0700

    add query article & category

---
+++ mysql/mysql-query.js
@@ -0,0 +1,56 @@
+var SQLconnection = require('./mysql-connector');
+var mysql = require('mysql2');
+var db;
+module.exports = {
+    //
+    getUserAccount: function(callback){
+        SQLconnection.connectToServer();
+        db = SQLconnection.getConnectionInstance();
+        db.query('SELECT * FROM useraccount', function(err,rows){
+            if(err) throw err;
+            return callback(null,rows);
+        });
+    },
+
+    getAPISource:function(callback){
+        SQLconnection.connectToServer();
+        db = SQLconnection.getConnectionInstance();
+        db.query('SELECT * FROM apisource',function(err,rows){
+            if(err) throw err;
+            return callback(null,rows);
+        });
+    },
+    getCategoryList: function(callback){
+        SQLconnection.connectToServer();
+        db = SQLconnection.getConnectionInstance();
+        db.query('SELECT * FROM category', function(err,rows){
+            if(err) throw err;
+            //return rows
+            return callback(null,rows);
+        });
+    },
+
+
+
+    // get Article
+    getArticle: function (callback) {
+        SQLconnection.connectToServer();
+        db = SQLconnection.getConnectionInstance();
+        db.query('SELECT * FROM article', function (err, rows) {
+            if (err) throw err;
+            //return rows;
+            return callback(null,rows);
+        });
+        //
+    },
+    getArticleInCategory: function(category, callback){
+        SQLconnection.connectToServer();
+        db = SQLconnection.getConnectionInstance();
+        db.query('SELECT * FROM article,category WHERE category.categoryName = ? AND article.Category = category.idCategory',
+                [category],function(err,rows){
+                    if(err) throw err;
+                    //return rows
+                    return callback(null,rows);
+                });
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var SQLquery = require('./mysql-query');
+var SQLquery = require('../mysql/mysql-query');
 
-/* GET API request. */
+router.get('/users', function (req, res) {
+    SQLquery.getUserAccount(function (err, data) {
+        if (err) throw err;
+        else
+            res.status(200).send(data);
+    });
+});
+
+
+/* Begin article query */
 router.get('/articles', function (req, res) {
     SQLquery.getArticle(function (err, data) {
         if (err) return;
@@ -10,6 +19,27 @@ router.get('/articles', function (req, res) {
             res.status(200).send(data);
     });
 });
+
+router.get('/articles/:category', function (req, res) {
+    let category = req.params.category;
+    SQLquery.getArticleInCategory(category, function (err, data) {
+        if (err) return err;
+        else
+            res.status(200).send(data);
+    });
+});
+
+router.get('/articles/:category/:keyword', function (req, res) {
+    let category = req.params.category;
+    let keyword = req.param(keyword);
+    if (keyword) {
+        console.log(keyword);
+    } else {
+        console.log(category);
+    }
+})
+
+
 router.get('/category', function (req, res) {
     SQLquery.getCategoryList(function (err, data) {
         if (err) return;
@@ -17,4 +47,13 @@ router.get('/category', function (req, res) {
             res.status(200).send(data);
     })
 });
+
+router.get('/apisource', function (req, res) {
+    SQLquery.getAPISource(function (err, data) {
+        if (err) return;
+        else
+            res.status(200).send(data);
+    })
+});
+
 module.exports = router;
